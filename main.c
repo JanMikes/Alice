a322aea0d97845a01622b297957fe844b0ac7e1b
Jan Mikeš <j.mikes@me.com>
2019-10-21 20:25:04 +0200

    Vice zapisu

--- main.c
+++ main.c
@@ -2,22 +2,28 @@
 
 int main()
 {
-    int a;
-    float b, vysledek;
+    int a, b;
+    float c, vysledek;
     
     printf("Zadej cele cislo: ");
     scanf("%d", &a);
     
-    printf("Zadej desetinne cislo: ");
-    scanf("%f", &b);
+    printf("Zadej cele cislo: ");
+    scanf("%d", &b);
+    
+    printf("Zadej realne cislo: ");
+    scanf("%f", &c);
     
     printf("Tvoje cele cislo je: %d\n", a);
-    printf("Tvoje desetinne cislo je: %5.2f\n", b);
+    printf("Tvoje desetinne cislo je: %5.2f\n", c);
     
-    vysledek = a/b;
+    vysledek = (float) a/ (float) b;
 
     printf("Vydelene cislo je: %5.2f\n", vysledek);
     printf("Semilogaritmicky tvar vysledku je: %e\n", vysledek);
+    
+    printf("Hexadecimalni A: %i\n", a);
+    printf("Oktadecimalni A: %o\n", a);
     
     return 0;
 }
