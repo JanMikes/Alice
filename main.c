83c55e30658d4c0b40b3ab72bc23a33c041ddca9
Jan Mikeš <j.mikes@me.com>
2019-10-21 20:11:27 +0200

    Deleni

--- main.c
+++ main.c
@@ -8,10 +8,13 @@ int main()
     printf("Zadej cele cislo: ");
     scanf("%d", &a);
     
-    
     printf("Zadej desetinne cislo: ");
     scanf("%f", &b);
     
     printf("Tvoje cele cislo je: %d\n", a);
     printf("Tvoje desetinne cislo je: %.2f\n", b);
+
+    printf("Vydelene cislo je: %.2f\n", a/b);
+    
+    return 0;
 }
